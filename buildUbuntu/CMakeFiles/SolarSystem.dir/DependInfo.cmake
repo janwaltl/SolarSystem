2168c456529f548b283ccff31866df3685ac580f
Jan Waltl(UnixVM) <waltl.jan@gmail.com>
2017-09-05 23:14:51 +0200

    Default build for Ubuntu

---
+++ buildUbuntu/CMakeFiles/SolarSystem.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/osboxes/Zapoctak/solar/SolarSystem/External/glad/glad.c" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/External/glad/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../SolarSystem"
+  "../SolarSystem/External"
+  "../glfw-3.2.1/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osboxes/Zapoctak/solar/SolarSystem/External/imgui/imgui.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/External/imgui/imgui.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/External/imgui/imguiExtended/LineGraph.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/External/imgui/imguiExtended/LineGraph.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/External/imgui/imgui_draw.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/External/imgui/imgui_draw.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Common/Timing.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Common/Timing.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/ConsoleControl.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/ConsoleControl.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Main.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Main.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Parsers/FormattedFileParser.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Parsers/FormattedFileParser.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Parsers/ReplayerParser.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Parsers/ReplayerParser.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Parsers/SolarParser.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Parsers/SolarParser.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/SimMethods/ExplicitEuler.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/SimMethods/ExplicitEuler.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/SimMethods/RK4.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/SimMethods/RK4.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/SimMethods/ReplayerMethod.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/SimMethods/ReplayerMethod.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/SimMethods/SemiImplicitEuler.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/SimMethods/SemiImplicitEuler.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Simulation.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Simulation.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Units/SystemUnit.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Units/SystemUnit.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Units/Unit.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Units/Unit.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/Graphs.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/Graphs.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/ObjectContextMenu.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/ObjectContextMenu.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/ReplayControls.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/ReplayControls.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/SimProperties.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/SimProperties.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/UnitsProperties.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/UnitsProperties.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/VisualPreferences.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUI/VisualPreferences.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUIDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/GUIDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/ReplayGUIDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/ReplayGUIDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/Camera.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/Camera.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/CameraControls.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/CameraControls.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/GridDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/GridDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/LineTrailsDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/LineTrailsDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/SimDataDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/SimDataDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/TestDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/Scene/TestDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/SceneDrawer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/Drawers/SceneDrawer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/ImGuiBackend.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/ImGuiBackend.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/CircleBuffer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/CircleBuffer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Error.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Error.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Grid.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Grid.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Pinheads.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Pinheads.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Shader.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Shader.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Sphere.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/Sphere.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/UnitTrail.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGL/UnitTrail.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/IMGuiViewer/OpenGLBackEnd.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/IMGuiViewer/OpenGLBackEnd.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/ReplayerViewer.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/ReplayerViewer.cpp.o"
+  "/home/osboxes/Zapoctak/solar/SolarSystem/Source/Viewers/ViewAndRecord.cpp" "/home/osboxes/Zapoctak/solar/buildUbuntu/CMakeFiles/SolarSystem.dir/SolarSystem/Source/Viewers/ViewAndRecord.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SolarSystem"
+  "../SolarSystem/External"
+  "../glfw-3.2.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/osboxes/Zapoctak/solar/buildUbuntu/glfw-3.2.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
